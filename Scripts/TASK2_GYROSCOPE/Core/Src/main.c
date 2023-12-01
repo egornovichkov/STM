458378381e45f49077ab85773c57b5b96a77793d
egornovichkov <novichkovegor41@gmail.com>
2023-12-01 05:19:24 +0300

    TASK2_GYROSCOPE in progress

---
+++ Scripts/TASK2_GYROSCOPE/Core/Src/main.c
@@ -0,0 +1,368 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#define DUMMY_BYTE          ((uint8_t)0x00)
+#define READWRITE_CMD       ((uint8_t)0x80)
+#define MULTIPLEBYTE_CMD    ((uint8_t)0x40)
+#define SPIx_TIMEOUT_MAX                      ((uint32_t)0x1000)
+#define L3GD20_WHO_AM_I_ADDR  ((uint8_t)0x0F)
+#define L3GD20_OUT_X_L_ADDR   ((uint8_t)0x28)
+#define L3GD20_CTRL_REG1 	  ((uint8_t)0x20)
+
+#define GYRO_CS_LOW()       HAL_GPIO_WritePin(GPIOC, GYROSCOPE_CS_Pin, GPIO_PIN_RESET)
+#define GYRO_CS_HIGH()      HAL_GPIO_WritePin(GPIOC, GYROSCOPE_CS_Pin, GPIO_PIN_SET)
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI2_Init(void);
+
+static uint8_t SPIx_WriteRead(uint8_t byte);
+
+void GYRO_IO_Init(void);
+void GYRO_IO_Write(uint8_t *pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite);
+void GYRO_IO_Read(uint8_t *pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead);
+void GYRO_ReadXYZ_AngRate(float *Data);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+static uint8_t SPIx_WriteRead(uint8_t Byte)
+{
+
+	uint8_t receivedbyte = 0;
+
+	/* Send a Byte through the SPI peripheral */
+	/* Read byte from the SPI bus */
+	HAL_SPI_TransmitReceive(&hspi2, (uint8_t*) &Byte, (uint8_t*) &receivedbyte,
+			1, SPIx_TIMEOUT_MAX);
+
+	return receivedbyte;
+}
+
+void GYRO_IO_Write(uint8_t *pBuffer, uint8_t WriteAddr, uint16_t NumByteToWrite)
+{
+	/* Configure the MS bit:
+	 - When 0, the address will remain unchanged in multiple read/write commands.
+	 - When 1, the address will be auto incremented in multiple read/write commands.
+	 */
+	if (NumByteToWrite > 0x01)
+	{
+		WriteAddr |= (uint8_t) MULTIPLEBYTE_CMD;
+	}
+	/* Set chip select Low at the start of the transmission */
+	GYRO_CS_LOW();
+
+	/* Send the Address of the indexed register */
+	SPIx_WriteRead(WriteAddr);
+
+	/* Send the data that will be written into the device (MSB First) */
+	while (NumByteToWrite >= 0x01)
+	{
+		SPIx_WriteRead(*pBuffer);
+		NumByteToWrite--;
+		pBuffer++;
+	}
+
+	/* Set chip select High at the end of the transmission */
+	GYRO_CS_HIGH();
+}
+
+void GYRO_IO_Read(uint8_t *pBuffer, uint8_t ReadAddr, uint16_t NumByteToRead)
+{
+	if (NumByteToRead > 0x01)
+	{
+		ReadAddr |= (uint8_t) (READWRITE_CMD | MULTIPLEBYTE_CMD);
+	}
+	else
+	{
+		ReadAddr |= (uint8_t) READWRITE_CMD;
+	}
+	/* Set chip select Low at the start of the transmission */
+	GYRO_CS_LOW();
+
+	/* Send the Address of the indexed register */
+	SPIx_WriteRead(ReadAddr);
+
+	/* Receive the data that will be read from the device (MSB First) */
+	while (NumByteToRead > 0x00)
+	{
+		/* Send dummy byte (0x00) to generate the SPI clock to GYROSCOPE (Slave device) */
+		*pBuffer = SPIx_WriteRead(DUMMY_BYTE);
+		NumByteToRead--;
+		pBuffer++;
+	}
+
+	/* Set chip select High at the end of the transmission */
+	GYRO_CS_HIGH();
+}
+
+void GYRO_ReadXYZ_AngRate(float *Data)
+{
+	float sensitivity = 0.00875;
+	uint8_t tmpbuffer[6] =
+	{ 0 };
+	int16_t RawData[3] =
+	{ 0 };
+
+	GYRO_IO_Read(tmpbuffer, L3GD20_OUT_X_L_ADDR, 6);
+
+	for (int i = 0; i < 3; i++)
+	{
+		RawData[i] = (int16_t) (((uint16_t) tmpbuffer[2 * i] << 8)
+				+ tmpbuffer[2 * i + 1]);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		Data[i] = (float) (RawData[i] * sensitivity);
+	}
+}
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+	/* USER CODE BEGIN 1 */
+
+	/* USER CODE END 1 */
+
+	/* MCU Configuration--------------------------------------------------------*/
+
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* USER CODE BEGIN Init */
+
+	/* USER CODE END Init */
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* USER CODE BEGIN SysInit */
+
+	/* USER CODE END SysInit */
+
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_SPI2_Init();
+	/* USER CODE BEGIN 2 */
+
+	// Включаем оси
+	uint8_t ctrl = 0x0f;
+	GYRO_IO_Write(&ctrl, L3GD20_CTRL_REG1, 1);
+
+	/* USER CODE END 2 */
+	uint8_t address_whoami = 0x0F;
+	uint8_t data;
+
+	GYRO_IO_Read(&data, address_whoami, 1);
+
+	float Data_AngRate[3] = {0};
+	float Data_Ang[3] = {0};
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+		/* USER CODE END WHILE */
+		GYRO_ReadXYZ_AngRate(Data_AngRate);
+		for (int i = 0; i < 3; ++i)
+		{
+			Data_Ang[i] = Data_AngRate[i] * 0.02;
+		}
+		HAL_Delay(20);
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct =
+	{ 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct =
+	{ 0 };
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48;
+	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI48;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI2_Init(void)
+{
+
+	/* USER CODE BEGIN SPI2_Init 0 */
+
+	/* USER CODE END SPI2_Init 0 */
+
+	/* USER CODE BEGIN SPI2_Init 1 */
+
+	/* USER CODE END SPI2_Init 1 */
+	/* SPI2 parameter configuration*/
+	hspi2.Instance = SPI2;
+	hspi2.Init.Mode = SPI_MODE_MASTER;
+	hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+	hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+	hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+	hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi2.Init.NSS = SPI_NSS_SOFT;
+	hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+	hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	hspi2.Init.CRCPolynomial = 7;
+	hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+	hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+	if (HAL_SPI_Init(&hspi2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+	/* USER CODE BEGIN SPI2_Init 2 */
+
+	/* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct =
+	{ 0 };
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
+
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GYROSCOPE_CS_GPIO_Port, GYROSCOPE_CS_Pin, GPIO_PIN_RESET);
+
+	/*Configure GPIO pin : GYROSCOPE_CS_Pin */
+	GPIO_InitStruct.Pin = GYROSCOPE_CS_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+	HAL_GPIO_Init(GYROSCOPE_CS_GPIO_Port, &GPIO_InitStruct);
+
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
